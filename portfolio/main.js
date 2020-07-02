442364aa160afdd926c2cd37a0d92ae76b462584
shivika24 <43493690+shivika24@users.noreply.github.com>
2020-07-02 13:35:11 +0100

    Add files via upload

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".type", {
-    strings: ["a Developer","a Student", "Shivika Singla"],
+    strings: ["a Developer","a Designer", "Shivika Singla"],
      typeSpeed:60,
      backSpeed:60,
      loop:true
@@ -10,4 +10,6 @@ var typed = new Typed(".type", {
         var body = document.getElementById("body");
         var currentClass = body.className;
         body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+        var t=document.getElementById("ii");
+        t.className=currentClass == "dark-mode" ?"uil uil-sun":"uil uil-moon"
       }
